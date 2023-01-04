62580370f2710de0bddc517ed492746fce03de18
fidanmzade <fidanm3zade@gmail.com>
2023-01-04 23:38:48 +0400

    yekun layihe

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>Snowdrop Shop</h1>
+            <p>Snowdrop, which makes the online shopping experience easier with each passing day and makes its services even more advantageous with developments in line with the requirements of the digitalized world, offers its visitors a wide variety, reasonable prices, fast delivery and surprise discounts.</p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>Azerbaijan, Baku, Nizami st.63</li>
+              <li>Email: example.help@gmail.com</li>
+              <li>Phone: +994(50)456 88 23</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
